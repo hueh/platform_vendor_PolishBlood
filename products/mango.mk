48169ba31d5ff6c4f069e4eebafec20168bb65f6
Przemysław Błaszków <pblaszkow5@gmail.com>
2013-05-29 13:19:25 +0200

    Add mango device, and small changes to coconut

--- products/mango.mk
+++ products/mango.mk
@@ -1,5 +1,5 @@
-# Inherit AOSP device configuration for coconut
-$(call inherit-product-if-exists, device/semc/coconut/full_coconut.mk)
+# Inherit AOSP device configuration for mango
+$(call inherit-product-if-exists, device/semc/mango/full_mango.mk)
 
 # Inherit common device configuration for PolishBlood Project 
 $(call inherit-product, vendor/PolishBlood/common_PolishBlood.mk)
@@ -8,24 +8,17 @@ $(call inherit-product, vendor/PolishBlood/common_PolishBlood.mk)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
 # Setup device specific product configuration.
-PRODUCT_NAME := PolishBlood_coconut
-PRODUCT_BRAND := Sony
-PRODUCT_DEVICE := coconut
-PRODUCT_MODEL := Live With Walkman
+PRODUCT_NAME := PolishBlood_mango
+PRODUCT_BRAND := SEMC
+PRODUCT_DEVICE := mango
+PRODUCT_MODEL := Xperia Mini Pro
 PRODUCT_MANUFACTURER := SEMC
 
 #skip asserts for now
 TARGET_OTA_ASSERT_SKIP := true
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT11i BUILD_FINGERPRINT="SEMC/WT19i_1254-2184/Wt19i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="WT19i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SK17i BUILD_FINGERPRINT="SEMC/SK17i_1254-2184/SK17i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="SK17i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
 
 PRODUCT_PACKAGES += \
     Mms \
-
-
-
-
-
-
-
