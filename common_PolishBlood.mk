0b680aa71d64a9ead4bbee3127b2b75c36fd7348
Przemysław Błaszków <pblaszkow5@gmail.com>
2013-05-30 15:31:28 +0200

    Compatibility with GooManager
    It should now be compatible with GooManager - faster and easier way to update ROMs !

--- common_PolishBlood.mk
+++ common_PolishBlood.mk
@@ -1,5 +1,7 @@
 # user, just user
-USER := pcfighter
+USER := pcf-$(DATE)
+# Version
+PBP_VERSION_NUMBER := 1
 
 # Unofficial packages that I used from trees outside AOSP
 PRODUCT_PACKAGES += \
@@ -10,3 +12,9 @@ PRODUCT_PACKAGES += \
 	
 PRODUCT_COPY_FILES += \
 	vendor/PolishBlood/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Goo updater app
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=pcfighter \
+    ro.goo.rom=pbp \
+    ro.goo.version=$(PBP_VERSION_NUMBER)
